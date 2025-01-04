3a1ae8a11fd5599341161df1673ab9ab7ca4539e
Zach Weed <zachweed@gmail.com>
2025-01-03 21:17:46 -0500

    add complex plane spec

--- spec/complex_plane_spec.rb
+++ spec/complex_plane_spec.rb
@@ -3,7 +3,7 @@ require './complex_plane.rb'
 
 RSpec.describe ComplexPlane do
   context '#generate_y_points' do
-    let(:points){ ComplexPlane.new().send(:generate_y_points) }
+    let(:points){ ComplexPlane.new.send(:generate_y_points) }
     it 'builds a list of y points that can exist on a complex plane' do
       expect(points.class).to eq(Array)
     end
